6f7bf22e095add53ebc3055202c67aa796653ce8
Sakshi <sakshi.sahlots@gmail.com>
2024-07-11 11:48:34 +0530

    added education section

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie'), // the DOM element to render the animation
+    renderer: 'svg', // the rendering type (svg/canvas/html)
+    loop: true, // whether the animation should loop
+    autoplay: true, // whether the animation should start automatically
+    path: 'json/animationJSON.json' // the path to the animation json
+});
   
