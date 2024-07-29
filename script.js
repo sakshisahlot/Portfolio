e15326061666f219d6578f8b2521d3934801c7c1
Sakshi <sakshi.sahlots@gmail.com>
2024-07-29 22:07:52 +0530

    Contact form and footer

--- script.js
+++ script.js
@@ -38,3 +38,48 @@ document.addEventListener('DOMContentLoaded', () => {
         overlay.style.opacity = '0';
     });
 });
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const serviceID = 'service_eryrojr';
+  const templateID = 'template_7ezzfzm';
+  const templateParams = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value
+  };
+
+  emailjs.send(serviceID, templateID, templateParams)
+      .then(response => {
+          document.getElementById('responseMessage').textContent = 'Message sent successfully!';
+          document.getElementById('responseMessage').style.color = 'white';
+          document.getElementById('contactForm').reset();
+      })
+      .catch(error => {
+          document.getElementById('responseMessage').textContent = `Failed to send message: ${error.text}`;
+          document.getElementById('responseMessage').style.color = 'red';
+      });
+});
+
